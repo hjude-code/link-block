b2fe93c7215518f9b4dea0155b51491c752b55ec
hjude-design <harrison@hjude.com>
2023-12-05 14:48:09 -0500

    addded attributes to render.php

--- build/render.php
+++ build/render.php
@@ -2,7 +2,21 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+ $IconURL = "src=".$attributes['Icon'];
+ $BlockLink = "href=".$attributes['Link'];
+
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Link-block – hello from a dynamic block!', 'link-block' ); ?>
-</p>
+<div <?php echo get_block_wrapper_attributes(); ?>>
+	<a <?php echo($BlockLink); ?> >
+		<div class="IconBar">
+			<img <?php echo($IconURL); ?>>
+		</div>
+		<h3>
+		<?php echo($attributes['Headline']); ?>
+		</h3>
+		<h3>
+		<?php echo($attributes['Location']); ?>
+		</h3>
+	</a>
+</div>
